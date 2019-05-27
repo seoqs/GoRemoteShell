9beb2f6a0cbc32a305e43926024d72e660d48470
seoqs <50579294+seoqs@users.noreply.github.com>
2019-05-28 00:57:28 +1000

    Client part.
    Сlient part sends teams of a server part and makes a conclusion of the executed commands.

---
+++ clShell.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+var servIPAddress = "127.0.0.1"
+
+func main() {
+	conn, _ := net.Dial("tcp", servIPAddress+":8081")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Command to send: ")
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(conn, "/c "+text+"\n")
+		connbuf := bufio.NewReader(conn)
+		for {
+			str, _ := connbuf.ReadString('\n')
+			if strings.Index(str, "<<<endMessage>>") > 0 {
+				break
+			}
+			fmt.Print(str)
+		}
+	}
+}
